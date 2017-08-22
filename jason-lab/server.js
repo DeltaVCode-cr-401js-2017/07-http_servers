afd85069d73b141b4fcf568bf8c0108d2a8aff50
nerdbetter <jasontlogan@gmail.com>
2017-08-22 12:06:28 -0500

    basic http GET server set up

---
+++ jason-lab/server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const http = require('http');
+const PORT = process.env.port || 3000;
+
+const url = require('url');
+const querystring = require('querystring');
+
+const server = http.createServer((req, res) =>{
+  console.log('req.url', req.url);
+  console.log('req.method', req.method);
+
+  req.url = url.parse(req.url);
+  console.log('url', req.url);
+
+  req.url.query = querystring.parse(req.url.query);
+  console.log('qs', req.url.query);
+
+  res.end();
+});
+
+server.listen(PORT, () =>{
+  console.log(`HTTP listening on ${PORT}`);
+});
