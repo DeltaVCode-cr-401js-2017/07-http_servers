bbcec9923712301fe28a4c468f4483be83fbcea8
nerdbetter <jasontlogan@gmail.com>
2017-08-22 16:00:09 -0500

    get requests for / and /cowsay operate correctly

--- jason-lab/server.js
+++ jason-lab/server.js
@@ -16,8 +16,92 @@ const server = http.createServer((req, res) =>{
   req.url.query = querystring.parse(req.url.query);
   console.log('qs', req.url.query);
 
-  res.end();
+  bodyParser(req, (err, body) => {
+    if (err){
+      res.writeHead(500);
+      res.write(err.toString());
+    }
+
+    const cowsay = require('cowsay');
+    const reqContentType = req.headers['content-type'];
+
+    switch(reqContentType){
+      case 'application/json':
+        console.log(`${reqContentType}`, body);
+        try {
+          if(req.method === 'GET' && req.url.pathname === '/'){
+            res.writeHead(200, {
+              'Content-Type': 'text/plain',
+            });
+            res.write('Welcome to my server!');
+            res.end();
+          }
+          if(req.method === 'GET' && req.url.pathname === '/cowsay'){
+            if(!req.url.query.text){
+              res.writeHead(400);
+              res.write(cowsay.say({ text: 'bad request' }));
+              return res.end();
+            }
+            res.writeHead(200, {
+              'Content-Type': 'text/plain'
+            });
+            res.write(cowsay.say({text: req.url.query.text}));
+            return res.end();
+          }
+
+          if (req.method === 'POST' && req.url.pathname === '/cowsay') {
+            req.body = JSON.parse(body);
+
+            res.writeHead(200, {
+              'Content-Type': 'application/json',
+            });
+            res.write(cowsay.say({ text: stuff }));
+            res.end();
+          }
+        }
+        catch (err){
+          res.writeHead(400);
+          res.write(err.message);
+          console.log(err);
+          return res.end();
+        }
+        break;
+
+      case 'text/plain':
+        res.write(body);
+        console.log(`${reqContentType}`, body);
+        return res.end();
+    }
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write(req.url.toString());
+    console.log('body', body);
+
+    res.end();
+  });
 });
+
+function bodyParser(req, callback){
+  switch (req.method){
+    case 'POST':
+      var body = '';
+      req.on('data', buf =>{
+        body += buf.toString();
+        console.log(buf);
+      });
+      req.on('end', ()=>{
+        callback(null, body);
+      });
+      req.on('error', err =>{
+        callback(err);
+      });
+      break;
+    default:
+      req.body = null;
+      callback(null, null);
+
+  }
+}
 
 server.listen(PORT, () =>{
   console.log(`HTTP listening on ${PORT}`);
