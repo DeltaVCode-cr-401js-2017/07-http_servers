5a33be099df6c7b5caf0ee340f591f05d4ddc939
Dustin <dyelland@gmail.com>
2017-08-22 14:54:57 -0500

    server responds with "hello from my server string"

--- lab-dustin/server.js
+++ lab-dustin/server.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const http = require('http');
+const cowsay = require('cowsay');
 const url = require('url');
 const querystring = require('querystring');
 const PORT = process.env.PORT || 3000;
+
 
 const server = http.createServer((req,res) => {
   console.log('req.url', req.url);
@@ -15,27 +17,53 @@ const server = http.createServer((req,res) => {
   req.url.query = querystring.parse(req.url.query);
   console.log('qs',req.url.query);
 
-  bodyParser(req,(err,body) => {
-    console.log('body', body);
-    
+
+  bodyParser(req,(err,body,message) => {
+    if (message) { console.log(message); }
+    const reqContentType = req.headers['content-type'];
+    switch (reqContentType){
+      case 'application/json':
+        try {
+          req.body = JSON.parse(body);
+          console.log('json',body);
+          res.write(message);
+        } catch (err) {
+          res.writeHead(400);
+          res.write(err.message);
+          return res.end();
+        }
+        break;
+      case 'text/plain':
+        break;
+      default:
+        console.warn(`Unexpected content-type: ${reqContentType}`);
+        res.writeHead(400);
+        return res.end();
+    }
+
     res.end();
   });
 });
 
 function bodyParser(req,callback){
   switch (req.method){
-    case "POST":
-      //
-      break;
-    case "PUT":
-      //
+    case 'POST':
+    case 'PUT':
+      var body = '';
+
+      req.on('data',buf => {
+        body += buf.toString();
+      });
+
+      req.on('end',() => callback(null,body,'hello from my server!'));
+      req.on('error', err => callback(err));
       break;
     default:
-    req.body = null;
+      req.body = null;
       callback(null,null);
   }
 }
 
 server.listen(PORT, () => {
-  console.log(`HTTP listening on ${PORT}.`)
+  console.log(`HTTP listening on ${PORT}.`);
 });
