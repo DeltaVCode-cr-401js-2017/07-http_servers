17ac9dd9c0bbf481e426c43876a5ffb32eb0cfa4
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-08-22 14:56:08 -0500

    parse url and querystring

--- lab-beeksma/server.js
+++ lab-beeksma/server.js
@@ -2,11 +2,19 @@
 
 const http = require('http');
 const PORT = process.env.PORT || 3000;
-
+const url = require('url');
+const querystring = require('querystring');
 
 const server = http.createServer((req, res) => {
   console.log('req.url', req.url);
   console.log('req.method', req.method);
+
+  req.url = url.parse(req.url);
+  console.log('url', req.url);
+
+  req.url.query = querystring.parse(req.url.query);
+  console.log('qs', req.url.query);
+
   res.end();
 });
 
